420c5b24b6e2aa50244c6af3d7c1f7b27fc4d254
leninner <mazabandalenin180@gmail.com>
2021-12-03 12:32:45 -0500

    [UPDATE]

--- api/index.js
+++ api/index.js
@@ -15,43 +15,44 @@ app.use(cors());
 
 // auth middleware
 const auth = jwt({
-	secret: process.env.JWT_SECRET,
-	credentialsRequired: false,
+  secret: process.env.JWT_SECRET,
+  credentialsRequired: false,
 });
 
 require('./adapter');
 
 const server = new ApolloServer({
-	introspection: true, // do this only for dev purposes
-	playground: true, // do this only for dev purposes
-	typeDefs,
-	resolvers,
-	context: ({ req }) => {
-		const { id, email } = req.user || {};
-		return { id, email };
-	},
+  introspection: true, // do this only for dev purposes
+  playground: true, // do this only for dev purposes
+  typeDefs,
+  resolvers,
+
+  context: ({ req }) => {
+    const { id, email } = req.user || {};
+    return { id, email };
+  },
 });
 
 app.use(auth);
 
 const errorHandler = (err, req, res, next) => {
-	if (res.headersSent) {
-		return next(err);
-	}
-	const { status } = err;
-	res.status(status).json(err);
+  if (res.headersSent) {
+    return next(err);
+  }
+  const { status } = err;
+  res.status(status).json(err);
 };
 app.use(errorHandler);
 server.applyMiddleware({ app, path: '/graphql' });
 
 app.get('/categories', function (req, res) {
-	res.send(categories);
+  res.send(categories);
 });
 
 if (!process.env.NOW_REGION) {
-	app.listen(PORT, () => {
-		console.log(`Listening at http://localhost:${PORT}/graphql`);
-	});
+  app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}/graphql`);
+  });
 }
 
 module.exports = app;
