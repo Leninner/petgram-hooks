8a19deae28c906101019e01d01294d06fd86408c
leninner <mazabandalenin180@gmail.com>
2021-12-03 13:27:40 -0500

    [UPDATE]

---
+++ src/components/Category/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Anchor = styled.a`
+	display: flex;
+	flex-direction: column;
+	text-align: center;
+	text-decoration: none;
+	width: 75px;
+`;
+
+export const Image = styled.img`
+	border: 1px solid #e6e6e6;
+	box-shadow: 0px 10px 14px rgba(0, 0, 0, 0.2);
+	border-radius: 50%;
+	height: auto;
+	object-fit: cover;
+	overflow: hidden;
+	height: 75px;
+	width: 75px;
+`;
