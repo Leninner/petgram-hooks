9c10cd204aa2f2fd2ee9664503399bcc83d45f6f
leninner <mazabandalenin180@gmail.com>
2021-12-06 15:52:18 -0500

    [UPDATE]

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Category } from '../Category';
 import { List, Item } from './style';
+import { categories } from '../../../api/db.json';
 
 export const ListOfCategories = () => {
 	return (
 		<List>
-			{[1, 2, 3, 4].map((value) => (
-				<Item key={value}>
-					<Category />
+			{categories.map((category) => (
+				<Item key={category.id}>
+					<Category {...category} />
 				</Item>
 			))}
 		</List>
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-  ],
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html'),
+		}),
+	],
 };
