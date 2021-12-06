b39ba725e41b6acc00c3d639ffc3f91d09b1962f
leninner <mazabandalenin180@gmail.com>
2021-12-06 18:12:16 -0500

    Update

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './style';
-import { categories } from '../../../api/db.json';
+import axios from 'axios';
 
 export const ListOfCategories = () => {
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		axios('https://petgram-server-leninner.vercel.app/categories').then((response) => {
+			setCategories(response.data);
+		});
+	}, []);
+
 	return (
 		<List>
 			{categories.map((category) => (
