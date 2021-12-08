9aac2a353033117cc6abdbb22ebc415f03bceb9d
leninner <mazabandalenin180@gmail.com>
2021-12-08 16:22:10 -0500

    Lazy loading

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { ImgWrapper, Img, Button } from './styles';
+import React, { useEffect, useRef, useState } from 'react';
+import { ImgWrapper, Img, Button, Article } from './styles';
 import { MdFavoriteBorder } from 'react-icons/md';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg';
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-	return (
-		<article>
-			<a href={`/detail/${id}`}>
-				<ImgWrapper>
-					<Img src={src} alt={id} />
-				</ImgWrapper>
-			</a>
+	const imgRef = useRef(null);
+	const [show, setShow] = useState(false);
 
-			<Button>
-				<MdFavoriteBorder size='32px' />
-				{likes} likes!
-			</Button>
-		</article>
+	useEffect(() => {
+		const observer = new window.IntersectionObserver((entries) => {
+			const { isIntersecting } = entries[0];
+			if (isIntersecting) {
+				console.log('Si');
+				setShow(true);
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(imgRef.current);
+	}, [imgRef]);
+
+	return (
+		<Article ref={imgRef}>
+			{show && (
+				<>
+					{' '}
+					<a href={`/detail/${id}`}>
+						<ImgWrapper>
+							<Img src={src} alt={id} />
+						</ImgWrapper>
+					</a>
+					<Button>
+						<MdFavoriteBorder size='32px' />
+						{likes} likes!
+					</Button>
+				</>
+			)}
+		</Article>
 	);
 };
