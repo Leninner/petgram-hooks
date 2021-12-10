30d8f4d58f1e71bd5f4d49f9f2c483b1894b7ea4
leninner <mazabandalenin180@gmail.com>
2021-12-10 17:03:14 -0500

    Update apollo y graph QL

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { PhotoCard } from '../PhotoCard';
+import { gql, useQuery } from '@apollo/client';
 
+const GET_PHOTOS = gql`
+	query getPhotos {
+		photos {
+			id
+			categoryId
+			src
+			likes
+			userId
+			liked
+		}
+	}
+`;
 export const ListOfPhotoCards = () => {
+	const { loading, error, data } = useQuery(GET_PHOTOS);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error!!!</p>;
+
 	return (
 		<ul>
-			{[
-				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-			].map((value) => (
-				<PhotoCard key={value} id={value} />
+			{data.photos.map((value) => (
+				<PhotoCard key={value.id} {...value} />
 			))}
 		</ul>
 	);
