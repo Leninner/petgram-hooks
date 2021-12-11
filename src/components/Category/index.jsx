a9baa8c16e092c75c869f5bf4e2cd9e575c1804a
leninner <mazabandalenin180@gmail.com>
2021-12-11 12:00:24 -0500

    Update

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -5,8 +5,8 @@ import { Image } from './styles';
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg';
 
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-	<Anchor href={path}>
-		<Image src={cover} alt={emoji} />
-		{emoji}
-	</Anchor>
+  <Anchor href={path}>
+    <Image src={cover} alt={emoji} />
+    {emoji}
+  </Anchor>
 );
