70f33a05de1caf9e92131c0e67bcc534287cd84a
leninner <mazabandalenin180@gmail.com>
2022-01-03 10:32:11 -0500

    Update

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,21 +3,23 @@ import { Route, Routes } from 'react-router-dom';
 import { User } from '../pages/User';
 import { Favs } from '../pages/Favs';
 import { NotRegisteredUser } from '../pages/NotRegisteredUser';
+import { NotFound } from '../pages/NotFound';
 
 export const PrivateRoute = ({ isAuth }) => {
   return (
-    <>
+    <Routes>
       {isAuth ? (
-        <Routes>
+        <>
           <Route path='/user' element={<User />} />
           <Route path='/favs' element={<Favs />} />
-        </Routes>
+        </>
       ) : (
-        <Routes>
+        <>
           <Route path='/user' element={<NotRegisteredUser />} />
           <Route path='/favs' element={<NotRegisteredUser />} />
-        </Routes>
+        </>
       )}
-    </>
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 };
