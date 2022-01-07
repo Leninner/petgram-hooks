b7e6f6c35f773e7b746da6f0ce3f3d5f39c4bfc2
leninner <mazabandalenin180@gmail.com>
2022-01-07 15:47:58 -0500

    Update

--- src/components/FavButton/index.jsx
+++ src/components/FavButton/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 import { Button } from './styles';
+import PropTypes from 'prop-types';
 
 function FavButton({ liked, likes, onClick }) {
   const Icon = liked ? MdFavorite : MdFavoriteBorder;
@@ -11,5 +12,11 @@ function FavButton({ liked, likes, onClick }) {
     </Button>
   );
 }
+
+FavButton.propTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default FavButton;
