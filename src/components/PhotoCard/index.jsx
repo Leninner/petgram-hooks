6a7dc7a7926d283d1811283df1d7ed8186df4c60
leninner <mazabandalenin180@gmail.com>
2022-01-07 17:24:48 -0500

    Update

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -4,6 +4,7 @@ import { useNearScreen } from '../../hooks/useNearScreen';
 import FavButton from '../FavButton';
 import { useToggleLikeMutation } from '../../hooks/useToggleLikeMutation';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg';
 
@@ -33,4 +34,23 @@ export const PhotoCard = ({ id, likes, liked = 0, src = DEFAULT_IMAGE }) => {
       )}
     </Article>
   );
+};
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  src: PropTypes.string,
+  likes: (props, propName, componentName) => {
+    const propValue = props[propName];
+
+    if (propValue === undefined) {
+      return new Error(`${propName} value must be defined`);
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName} value must be greater than 0`);
+    }
+
+    return null;
+  },
+  liked: PropTypes.bool.isRequired,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,65 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.jsx',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'public', 'index.html'),
-		}),
-	],
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'The Petgram App',
+      short_name: 'Petgram',
+      description: 'Con esta aplicación puedes encontrar fotos de animales muy cute',
+      background_color: '#ffffff',
+      theme_color: '#b1a',
+      crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          purpose: 'maskable', // <-- Añade esta línea
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          },
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-leninner-leninner.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+          },
+        },
+      ],
+    }),
+  ],
 };
